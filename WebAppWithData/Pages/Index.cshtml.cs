ed2b72bb0397e0d4bac347f315fbf7a9d1cb5a09
samee <samee@SAMEEL_FAIROOSE>
2022-08-13 00:10:59 -0500

    Add project files.

---
+++ WebAppWithData/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppWithData.Models;
+using WebAppWithData.Services;
+
+namespace WebAppWithData.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public List<Product> Products = null;
+
+        public void OnGet()
+        {
+            ProductService prodService = new ProductService();
+            Products = prodService.GetProducts();
+        }
+    }
+}
---
+++ WebAppWithData/Services/ProductService.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlClient;
+using WebAppWithData.Models;
+
+namespace WebAppWithData.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "appdbserverdev.database.windows.net";
+        private static string db_user = "sameel";
+        private static string db_password = "astech@j1d5qN";
+        private static string db_database = "appdb";
+
+        private SqlConnection GetConnectionString()
+        {
+            string connectionString = string.Empty;
+            var _connectionBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = db_source,
+                UserID = db_user,
+                Password = db_password,
+                InitialCatalog = db_database,
+            };
+            return new SqlConnection(_connectionBuilder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection connection = GetConnectionString();
+            List<Product> products = new List<Product>();
+            string query = "SELECT * FROM Products";
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    products.Add(
+                        new Product()
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Quantity = reader.GetInt32(2)
+                        }
+                    );
+                }
+                connection.Close();
+                return products;
+            }
+        }
+    }
+}
